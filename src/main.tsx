2bdd2eeed2811a150d339ad1306bae6e07351b03
JosiasAurel <ndjosiasaurel@gmail.com>
2023-08-20 09:46:01 +0100

    use app as-is

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import AppController from "./controller";
+import App from "./App";
 import "./index.css";
 import { GeistProvider, CssBaseline } from "@geist-ui/core";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <GeistProvider>
     <React.StrictMode>
-      <AppController />
+      <App />
     </React.StrictMode>
   </GeistProvider>,
 );
